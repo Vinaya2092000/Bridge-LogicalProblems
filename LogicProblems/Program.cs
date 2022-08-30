dbc713bca464f5dc7c5efd04af932c97f6e6f219
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-30 09:21:04 +0530

    Welcome

---
+++ LogicProblems/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LogicProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("1 for Fibonacci Series");
+            Console.WriteLine("2 for Perfect number");
+            Console.WriteLine("3 for Prime Number");
+            Console.WriteLine("4 for Reverse Number");
+            Console.WriteLine("5 for Coupon Number");
+            Console.WriteLine("6 for Stop Watch");
+
+            int Prog = Convert.ToInt32(Console.ReadLine());
+            switch (Prog)
+            {
+                default:
+                    break;
+            }
+        }
+    }
+}
